fc176aac9f16b65336176440688cccc5c0398904
Paul B <69534608+paulbgomez@users.noreply.github.com>
2022-02-06 17:58:00 +0100

    Added eclipse Suma Resta class
    - Added .DS_Store to gitignore

---
+++ Practica2Sistemas/src/Practica2Sistemas/Resta.java
@@ -0,0 +1,81 @@
+package practica1Sistemas;
+
+import java.io.IOException;
+
+/**
+ * <h2>Clase Resta para poder realizar restas</h2>
+ * @author paulbarrosogomez
+ * @version 1.0
+ */
+public class Resta {
+	
+	/**
+	 * Atributo de clase para acumular restas
+	 */
+	int classNumber = 0;
+	
+	/**
+	 * Metodo para comprobar que el numero introducido no sea menor a 0
+	 * @param arg numero a checkear
+	 * @throws IOException no se aceptan numeros negativos
+	 */
+	public void checkArgValue (int arg) throws IOException {
+		if (arg < 0) throw new IOException("no se aceptan numeros negativos");    
+	}
+	
+	/**
+	 * Metodo para restar dos numeros naturales
+	 * @param arg1 primer numero entero de la resta
+	 * @param arg2 segundo numero entero de la resta
+	 * @throws IOException error cuando los numeros introducidos son negativos
+	 * @return numero entero resultado de la resta
+	 */
+	public int restaReales(int arg1, int arg2) throws IOException {
+		checkArgValue(arg1);
+		checkArgValue(arg2);
+		var result = arg1 - arg2;
+		return result;
+	}
+	
+	/**
+	 * Sobrecarga de metodo para restar 3 numeros naturales
+	 * @param arg1 primer numero entero de la resta
+	 * @param arg2 segundo numero entero de la resta
+	 * @param arg3 tercer numero entero de la resta
+	 * @throws IOException error cuando los numeros introducidos son negativos
+	 * @return numero entero resultado de la resta
+	 */
+	public int restaReales(int arg1, int arg2, int arg3) throws IOException {
+		checkArgValue(arg1);
+		checkArgValue(arg2);
+		checkArgValue(arg3);
+		var result = arg1 - arg2 - arg3;
+		return result;
+	}
+	
+	/**
+	 * Metodo para restar 2 numeros enteros
+	 * @param arg1 primer numero entero de la resta
+	 * @param arg2 segundo numero entero de la resta
+	 * @throws IOException error cuando los numeros introducidos son negativos
+	 * @return numero entero resultado de la resta
+	 */
+	public float restaEnteros(float arg1, float arg2) throws IOException {
+		checkArgValue((int) arg1);
+		checkArgValue((int) arg2);
+		var result = arg1 - arg2;
+		return result;
+	}
+	
+	/**
+	 * Metodo para restar de manera acumulativa sobre un parametro de la clase
+	 * @param arg numero entero para la resta
+	 * @throws IOException error cuando los numeros introducidos son negativos
+	 * @return el valor acumulativo classNumber de la clase
+	 */
+	public int restaAcumulativa(int arg) throws IOException {
+		checkArgValue(arg);
+		classNumber = classNumber - arg;
+		return classNumber;
+	}
+}
---
+++ Practica2Sistemas/src/Practica2Sistemas/Sum.java
@@ -0,0 +1,67 @@
+package practica1Sistemas;
+import java.io.IOException;
+
+/**
+ * <h2>Clase Sum para poder realizar sumas</h2>
+ * @author paulbarrosogomez
+ * @version 1.0
+ */
+public class Sum {
+
+	/*
+	 * Atributo de clase para sumas acumulativas
+	 */
+		int classNumber = 0;
+		
+	/**
+	* Metodo para comprobar que el numero introducido no sea menor a 0
+	* @param arg numero a checkear
+	* @throws IOException no se capetan numeros negativos
+	*/
+	public void checkArgValue (int arg) throws IOException {
+		if (arg < 0) throw new IOException("no se aceptan numeros negativos");    
+	}
+		
+	/**
+	* Metodo para sumar todos los numeros naturales deseados
+	* @param numbers admite numeros naturales separados por una coma
+	* @throws IOException error cuando los numeros introducidos son negativos
+	* @return int resultado de la suma de los numeros introducidos
+	* 
+	*/
+	public int sumReales(Integer... numbers) throws IOException {
+		var result = 0;
+		for(int num : numbers) { 
+			checkArgValue(num);
+			result = result + num; 
+		}
+		return result;
+	}
+		
+	/**
+	* Metodo para sumar 2 numeros enteros
+	* @param arg1 primer numero entero
+	* @param arg2 segundo numero entero
+	* @throws IOException error cuando los numeros introducidos son negativos
+	* @return numero entero resultado de la suma
+	*/
+	public float sumEnteros(float arg1, float arg2) throws IOException {
+		checkArgValue((int) arg1);
+		checkArgValue((int) arg2);
+		var result = arg1 + arg2;
+		return result;
+	}
+		
+	/**
+	* Metodo que suma de manera acumulativa con un parametro de clase 
+	* @param arg numero entero para la suma
+	* @throws IOException error cuando los numeros introducidos son negativos
+	* @return el valor acumulativo classNumber de la clase
+	*/
+	public int sumAcumulativo(int arg) throws IOException {
+		checkArgValue(arg);
+		classNumber = classNumber + arg;
+		return classNumber;
+	}
+
+}
